51671166c20476d060ac20d51ecee049aaea865a
Aditya-arya105 <aryaaditya8989@gmail.com>
2025-05-11 10:48:18 +0100

    add a file and folder

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.auto-type').forEach(function(e) {
+    new Typed(e, {
+        strings: ["Web Developer", "Software Developer", "Coder"],
+        typeSpeed: 120,
+        backSpeed: 120,
+        loop: true
+    })
+})
